b45b1891358d3225f6fe3e26e6e8540b30ec3264
Vasily Shelkov <vasilydshelkov@gmail.com>
2015-10-02 14:43:05 +0100

    changing server.js again to try to get to start correctly on openshift

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(app.get('port') ,app.get('ip'), function (err, result) {
+}).listen(server_port, server_ip_address, function (err, result) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Listening at localhost:8080');
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port);
 });

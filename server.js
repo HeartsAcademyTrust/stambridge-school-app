c6e8336c5b188b10635749e9dcfa0fec4fe91b77
Vasily Shelkov <vasilydshelkov@gmail.com>
2015-10-02 14:33:46 +0100

    added app.set(port|ip) to server.js

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(8080, 'localhost', function (err, result) {
+}).listen(app.get('port') ,app.get('ip'), function (err, result) {
   if (err) {
     console.log(err);
   }

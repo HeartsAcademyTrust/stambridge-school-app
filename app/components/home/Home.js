3ed5a30241fe91cc51591da9e34de1d03bd5779f
Vasily Shelkov <vasilydshelkov@gmail.com>
2016-06-06 17:10:19 +0100

    Updated all the packages and changed Home to stateless function

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -2,24 +2,16 @@ import React from 'react';
 import About from './About';
 import CoreValues from './CoreValues';
 import LatestNews from './latest/LatestNews';
-import Gallery from './Gallery';
+// import Gallery from './Gallery';
 
-class Home extends React.Component{
-	render(){
-		return (
-			<div>
-				{/*<Gallery /> */}
-
-				<div className="divider"></div>
-
-				<LatestNews />
-
-				<About />
-
-				<CoreValues />
-			</div>
-		);
-	}
-}
+const Home = () => (
+  <div>
+		{/* <Gallery /> */}
+    <div className="divider"></div>
+    <LatestNews />
+    <About />
+    <CoreValues />
+  </div>
+);
 
 export default Home;
